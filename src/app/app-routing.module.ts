4fee92ecd7c1db61855ff321956bbbaaf290039a
Cristobal Bruna <cristobalbrunaferj@gmail.com>
2020-10-05 11:46:03 -0300

    initial angular app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
